3f614e1208bb279007248c71523e5ba7a5aee65b
suresh-3x <77671068+suresh-3x@users.noreply.github.com>
2021-04-20 21:49:53 +0530

    Create setup.py

---
+++ stridepip/setup.py
@@ -0,0 +1,14 @@
+  
+from setuptools import setup
+
+setup(
+    name='smax',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='https://github.com/suresh-3x/smax.git',
+    author='suresh-3x',
+    author_email='suresh.37x@gmail.com',
+    license='unlicense',
+    packages=['smax'],
+    zip_safe=False
+)
