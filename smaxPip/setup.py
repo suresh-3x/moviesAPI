df27b10a48b1add78cb91abd627f0d755fb731e5
suresh-3x <77671068+suresh-3x@users.noreply.github.com>
2021-04-20 14:44:06 +0530

    Create setup.py

---
+++ smaxPip/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='smaxPip',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='https://github.com/suresh-3x/smax.git',
+    author='suresh-3x',
+    author_email='suresh.37x@gmail.com',
+    license='unlicense',
+    packages=['smaxPip'],
+    zip_safe=False
+)
